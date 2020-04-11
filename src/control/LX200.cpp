f0589317fcfab41c59fad07e3794538ec138e424
Kevin <smeatsan@gmail.com>
2020-04-11 21:00:30 +0200

    Add a bunch of LX200 commands

--- src/control/LX200.cpp
+++ src/control/LX200.cpp
@@ -2,6 +2,7 @@
 
 #include "../core/clock.h"
 #include "../net/TCP.h"
+#include "RTClib.h"
 #include "stdint.h"
 
 #include <cstdlib>
@@ -18,6 +19,7 @@ void lx200_init(MountController* mc, Clock* c) {
 
 static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 	char return_msg[128];
+	uint8_t data_begin = 0; // used to skip unwanted " " sent by libindi :/
 	bool no_return = false;
 	return_msg[0] = 0;
 	if(len == 1 && msg[0] == 0x06) {
@@ -77,7 +79,7 @@ static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 				case 'P':
 					snprintf(return_msg, 128, "%s#", "none");
 					break;
-				// TODO: latitude
+				// latitude
 				case 't':
 					{
 						double latitude = LATITUDE;
@@ -86,6 +88,7 @@ static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 						snprintf(return_msg, 128, "%+02d*%02d#", deg, min);
 						break;
 					}
+				// longitude
 				case 'g':
 					{
 						double latitude = LONGITUDE;
@@ -95,19 +98,54 @@ static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 						break;
 					}
 					break;
-
+				// tracking rate TODO
+				case 'T':
+					snprintf(return_msg, 128, "60.0#");
+					break;
+				// UTC offset TODO
+				case 'G':
+					snprintf(return_msg, 128, "+00#");
+					break;
+				// current date
+				case 'C':
+					{
+						DateTime curr_time = rt_clock->get_time();
+						snprintf(return_msg, 128, "%02d/%02d/%02d", curr_time.month(), curr_time.day(), curr_time.year());
+					}
+					break;
+				// firmware stuff. currently all TODO
+				case 'V':
+					switch(msg[3]) {
+						case 'D':
+							snprintf(return_msg, 128, "011 11 2020#");
+							break;
+						case 'N':
+							snprintf(return_msg, 128, "11.1#");
+							break;
+						case 'P':
+							snprintf(return_msg, 128, "DIY#");
+							break;
+						case 'T':
+							snprintf(return_msg, 128, "11:11:11#");
+							break;
+						case 'F':
+							// undocumented :/ seems to be for a full version string
+							snprintf(return_msg, 128, "full version#");
+							break;
+					}
 					
 				default:
 					break;
 			}
 		break; // end case 'G'
 		case 'S':
+			data_begin = 3 + (msg[3] == ' '); // skip unwanted space XXX: only valid for one char commands
 			switch(msg[2]) {
 				case 'r':
 					{
-						int hours = (msg[4] - '0') * 10 + (msg[5] - '0');
-						int minutes = (msg[7] - '0') * 10 + (msg[8] - '0');
-						int seconds = (msg[10] - '0') * 10 + (msg[11] - '0');
+						int hours = (msg[data_begin] - '0') * 10 + (msg[data_begin + 1] - '0');
+						int minutes = (msg[data_begin + 3] - '0') * 10 + (msg[data_begin + 4] - '0');
+						int seconds = (msg[data_begin + 6] - '0') * 10 + (msg[data_begin + 7] - '0');
 						double ra = hours * 15 + minutes/4.0 + seconds/240.0;
 						mount_controller->set_target_ra(ra);
 						log_i("Moving ra to %f. %02d:%02d:%02d. msg was %s", ra, hours, minutes, seconds, msg);
@@ -116,10 +154,10 @@ static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 					break;
 				case 'd':
 					{
-						int sign = (msg[4] == '+') ? 1 : -1;
-						int deg = (msg[5] - '0') * 10 + (msg[6] - '0');
-						int minutes = (msg[8] - '0') * 10 + (msg[9] - '0');
-						int seconds = (msg[11] - '0') * 10 + (msg[12] - '0');
+						int sign = (msg[data_begin] == '+') ? 1 : -1;
+						int deg = (msg[data_begin + 1] - '0') * 10 + (msg[data_begin + 2] - '0');
+						int minutes = (msg[data_begin + 4] - '0') * 10 + (msg[data_begin + 5] - '0');
+						int seconds = (msg[data_begin + 7] - '0') * 10 + (msg[data_begin + 8] - '0');
 						double dec = sign * (deg + minutes/60.0 + seconds/3600.0);
 						mount_controller->set_target_dec(dec);
 						log_i("Moving dec to %f. %+02d*%02d:%02d. msg was %s", dec, deg, minutes, seconds, msg);
@@ -129,18 +167,51 @@ static void lx200_handle_single_message(uint8_t* msg, uint32_t len) {
 				// set date in stupid format MM/DD/YY
 				case 'C':
 					{
-						int month = (msg[4] - '0' * 10) + msg[5] - '0';
-						int day = (msg[7] - '0' * 10) + msg[8] - '0';
-						int year = 2000 + (msg[10] - '0' * 10) + msg[11] - '0';
+						int month = (msg[data_begin] - '0') * 10 + msg[data_begin + 1] - '0';
+						int day = (msg[data_begin + 3] - '0') * 10 + msg[data_begin + 4] - '0';
+						int year = 2000 + (msg[data_begin + 6] - '0') * 10 + msg[data_begin + 7] - '0';
 						DateTime old_date = rt_clock->get_time();
 						DateTime new_date(year, month, day, old_date.hour(), old_date.minute(), old_date.second());
 						rt_clock->sync(new_date);
-						log_i("New date is %d-%d-%d", year, month, day);
+						log_i("New date is %d-%d-%d from msg %s", year, month, day, msg);
 						// TODO: always valid for now
 						// the string is part of the specification.....
 						snprintf(return_msg, 128, "1Updating  Planetary Data#                                           #");
 					}
 				break;
+				// time
+				case 'L':
+				{
+					uint8_t hours = (msg[data_begin] - '0') * 10 + (msg[data_begin + 1] - '0');
+					uint8_t minutes = (msg[data_begin + 3] - '0') * 10 + (msg[data_begin + 4] - '0');
+					uint8_t seconds = (msg[data_begin + 6] - '0') * 10 + (msg[data_begin + 7] - '0');
+					DateTime old_date = rt_clock->get_time();
+					DateTime new_date(old_date.year(), old_date.month(), old_date.day(), hours, minutes, seconds);
+					rt_clock->sync(new_date);
+					log_i("New time is %d:%d:%d from msg %s", hours, minutes, seconds, msg);
+					snprintf(return_msg, 128, "1");
+				}
+				break;
+				// UTC offset TODO
+				case 'G':
+					snprintf(return_msg, 128, "1");
+					break;
+				// longitude
+				case 'g':
+				{
+					uint8_t sign = msg[data_begin] == '+' ? 1 : -1;
+					uint16_t degree = (msg[data_begin + 1] - '0') * 100 + (msg[data_begin + 2] - '0') * 10 + (msg[data_begin + 3] - '0');
+					uint8_t minutes = (msg[data_begin + 5] - '0') * 10 + (msg[data_begin + 6] - '0');
+					double longitude =sign * (degree + minutes/60.0);
+					log_i("Setting longitude to %f", longitude);
+					rt_clock->set_longitude(longitude);
+					snprintf(return_msg, 128, "1");
+				}
+				break;
+				// latitude TODO unused, but we never use it anyway
+				case 't':
+					snprintf(return_msg, 128, "1");
+					break;
 			}
 		break; // end case 'S'
 		case 'M':
